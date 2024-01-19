9cec7dd5420d8a57d5cfb5867181c1275f2cf73e
Artur Zakirov <zaartur@gmail.com>
2024-01-19 13:59:42 +0100

    Add more tests

--- test/sql/001_saturated_int.sql
+++ test/sql/001_saturated_int.sql
@@ -1,3 +1,8 @@
 create extension saturated_int;
 
+select 999999999999999::saturated_int;
 select 2147483648::saturated_int;
+select 2147483647::saturated_int;
+select (-2147483649)::saturated_int;
+select (-2147483648)::saturated_int;
+
